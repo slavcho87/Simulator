316ac573703fac4cb15452e665db7bf82ec3cb9b
Slavcho Ivanov <slavchoivanov@yahoo.es>
2016-01-07 22:38:13 +0100

    20160107-2237

--- public/javascripts/angular/controllers/StaticItemSettingsController.js
+++ public/javascripts/angular/controllers/StaticItemSettingsController.js
@@ -2,9 +2,23 @@ var app = angular.module("app");
 
 app.controller("StaticItemSettingsController", ['$scope', '$http', function ($scope, $http) {
     $scope.errorMsgList = [];
+    $scope.msgList = [];
+
     
 
-
+   /*
+    * Hide message
+    */    
+    $scope.msgSuccessHide = function(msg){
+        var msgIndex = $scope.msgList.indexOf(msg);
+        
+        if(msgIndex>=0){
+            $scope.msgList.splice(msgIndex, 1);
+        }else{
+            $scope.msgList.push(ERROR_HAS_OCCURRED);
+        }
+    }    
+    
    /*
     * Hide error message
     */    
